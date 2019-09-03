cc0e3e916f08e762f11dd97321fccb4a132415fa
maej <maej@maej-pc.wireless-1x.unc.edu>
2019-09-03 17:47:26 -0400

    completed assignment

--- src/a2/A2Novice.java
+++ src/a2/A2Novice.java
@@ -8,8 +8,101 @@ public class A2Novice {
 		Scanner scan = new Scanner(System.in);
 		
 		// Your code here.
+		
+		int NumIngredients = 0;
+		
+		String Is_Vegetarian = ""; 
+		int NumVeg = 0;
+		
+		
+		
+		
+		NumIngredients = scan.nextInt();
+		
+		String[] Ingredient_Name = new String[NumIngredients];
+		Double[] Price_Per_Ounce = new Double[NumIngredients];
+		Double[] Calories_Per_Ounce = new Double[NumIngredients];
+		
+		for(int i = 0; i < NumIngredients; i++) {
+			
+			Ingredient_Name[i] = scan.next();
+			Price_Per_Ounce[i] = scan.nextDouble();
+			Is_Vegetarian = scan.next();
+			Calories_Per_Ounce[i] = scan.nextDouble();
+			
+			if(Is_Vegetarian.compareTo("true")==0) {
+				
+				NumVeg++;
+				
+			}
+			
+			
+			
+		}
+		
+		System.out.println("Number of vegetarian ingredients:" + NumVeg);
+		System.out.println("Highest cals/$: " + Ingredient_Name[GetHighest(Calories_Per_Ounce,Price_Per_Ounce,NumIngredients)]);
+		System.out.println("Lowest cals/$: " + Ingredient_Name[GetLowest(Calories_Per_Ounce,Price_Per_Ounce,NumIngredients)]);
+		
+		
 	}
 	
 	// You can define helper methods here if needed.
 	
+	public static int GetHighest(Double[] a, Double[] b, int c) {
+		
+		int index = 0;
+		double highest = 0;
+		Double[] Ratios = new Double[c];
+		
+		for(int i = 0; i < c; i++) {
+			
+			Ratios[i] = a[i]/b[i];
+			
+		}
+		
+		for(int i = 0; i < c; i++) {
+			
+			if(highest>Ratios[i]) {
+				
+				highest = Ratios[i];
+				index = i;
+			}
+			
+			
+			
+		}
+		
+		return index; 
+	}
+	
+	
+public static int GetLowest(Double[] a, Double[] b, int c) {
+		
+		int index = 0;
+		
+		Double[] Ratios = new Double[c];
+		
+		for(int i = 0; i < c; i++) {
+			
+			Ratios[i] = a[i]/b[i];
+			
+		}
+		
+		double lowest = Ratios[0];
+		
+		for(int i = 0; i < c; i++) {
+			
+			if(lowest>Ratios[i]) {
+				
+				lowest = Ratios[i];
+				
+				index = i;
+				
+			}
+			
+		}
+		
+		return index; 
+	}
 }
